f6ba5e5ef6c6160f6a2579478ee160f8d2584133
qg7771481 <ddavidok845@gmail.com>
2026-01-12 20:42:07 +0100

    Update models.py

--- comments/models.py
+++ comments/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from posts.models import Post  # Імпорт з твого локального додатку
+from posts.models import Post  
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
